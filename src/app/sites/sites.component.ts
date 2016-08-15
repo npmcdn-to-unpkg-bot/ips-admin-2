4e3fa7640c33ad3560ec9a6767870deaeee08484
Paul <ajp01@acuitysso.com>
2016-08-15 08:04:15 -0400

    Merged with Kelly/Scott's code. Everything appears to be working except on the Sites tab you need to double click the All tab to load the site data. Part of Scott's site component may need to be merged back in.

--- src/app/sites/sites.component.ts
+++ src/app/sites/sites.component.ts
@@ -1,29 +1,16 @@
-import { Component, BreadcrumbComponent, ButtonAddComponent,
-  SitesAllComponent, SitesGroupsComponent, ROUTER_DIRECTIVES, MapComponent } from './sites';
+import { Component, ButtonAddComponent,
+  SitesAllContainer, SitesGroupsComponent, ROUTER_DIRECTIVES, MapComponent } from './sites';
 
 import './sites.component.less';
 
 @Component({
     selector: 'app-sites',
     templateUrl: '../src/app/sites/sites.component.html',
-    directives: [BreadcrumbComponent, ButtonAddComponent, SitesAllComponent,
+    directives: [ButtonAddComponent, SitesAllContainer,
                   SitesGroupsComponent, ROUTER_DIRECTIVES, MapComponent],
 })
 
 export class SitesComponent {
 
-  navHeader: string = "Sites";
-
-  allTabClick(): void {
-    this.navHeader = "Sites > All Sites";
-  }
-
-  groupTabClick(): void {
-    this.navHeader = "Sites > Site Groups";
-  }
-
-  favTabClick(): void {
-    alert("This view does not exist.");
-  }
         
 }
--- src/app/sites/sites.ts
+++ src/app/sites/sites.ts
@@ -2,7 +2,6 @@
 
 export { Component } from '@angular/core';
 export { ROUTER_DIRECTIVES } from '@angular/router';
-export { BreadcrumbComponent } from '../../shared/breadcrumbs/breadcrumbs.component';
 export { ButtonAddComponent } from '../../shared/buttonAdd/buttonAdd.component'; 
 export { SitesAllContainer } from '../sites-all/containers/sites-all.container';
 export { SitesGroupsComponent } from '../sites-groups/sites-groups.component';
