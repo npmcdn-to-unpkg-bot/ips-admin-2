d3d31891dd8136dd929dadd6b168f047766a5186
Paul <ajp01@acuitysso.com>
2016-08-17 15:33:47 -0400

    Changed colors for current design. Also fixed the package and vendor files to remove unused Material references.

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,7 +9,6 @@ export { AuthenticationComponent } from './authentication/authentication.compone
 //import { AuthRedirectComponent } from './authRedirect/authRedirect.component';
 export { MdButton } from '@angular2-material/button/button';
 export { MdToolbar } from '@angular2-material/toolbar/toolbar';
-export { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav/sidenav';
 export { MD_LIST_DIRECTIVES } from '@angular2-material/list/list';
 export { MdIcon, MdIconRegistry } from '@angular2-material/icon/icon';
 export { Dropdown } from './shared/dropdown/dropdown.component';
--- src/vendor.ts
+++ src/vendor.ts
@@ -4,12 +4,49 @@ import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
 import '@angular/http';
+import '@angular/router';
+import '@angular/forms';
+
+//Material
+import '@angular2-material/button';
+import '@angular2-material/card';
+import '@angular2-material/icon';
+import '@angular2-material/input';
+import '@angular2-material/list';
+import '@angular2-material/toolbar';
 
 // RxJS
-import 'rxjs';
+// Statics
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/share';
+import 'rxjs/operator/delay';
+import 'rxjs/operator/do';
+import 'rxjs/operator/filter';
+import 'rxjs/operator/finally';
+import 'rxjs/operator/multicast';
+import 'rxjs/operator/share';
 
 // NGRX
 import '@ngrx/store';
 
 // Leaflet
 import 'leaflet';
+
+//if ('production' === ENV) {
+  // Production
+//} else {
+  // Development
+  require('angular2-hmr');
+//}
